9f144e51afdd4f95393ccf97b814e18f25cc8d3e
francis <francisalbarracin2@gmail.com>
2026-07-26 11:31:24 +0900

    Make caller be able to specify how many times character is repeated

--- 03-iteration/repeat.go
+++ 03-iteration/repeat.go
@@ -2,11 +2,9 @@ package iteration
 
 import "strings"
 
-const repeatCount = 5
-
-func Repeat(character string) string {
+func Repeat(character string, repeatCount int) string {
 	var repeated strings.Builder
-	for i := 0; i < repeatCount; i++ {
+	for range repeatCount {
 		repeated.WriteString(character)
 	}
 	return repeated.String()
